478b331357a74bbf550e79fbf895c8dc7c663700
Shin Morisawa <pickstryhard143@gmail.com>
2024-10-20 15:55:22 +0900

    create main menu
    B
    C

---
+++ rhythmNexus.Game/Screens/Menus/MainMenu.cs
@@ -0,0 +1,58 @@
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osuTK;
+using osuTK.Graphics;
+
+public partial class MainMenu : CompositeDrawable {
+    public MainMenu() {
+        // Create a container to contain the buttons
+        var buttonContainer = new Container {
+            Size = new Vector2(200, 50),
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Position = new Vector2(0, 0)
+        };
+
+        buttonContainer.Add(new BasicButton {
+            Text = "Start",
+            Action = startGame,
+            Size = new Vector2(160, 40),
+            Position = new Vector2(0, 0),
+            Colour = Color4.LightGreen
+        });
+
+        buttonContainer.Add(new BasicButton {
+            Text = "Settings",
+            Action = openSettings,
+            Size = new Vector2(160, 40),
+            Position = new Vector2(180, 0),
+            Colour = Color4.Aqua
+        });
+
+        buttonContainer.Add(new BasicButton {
+            Text = "Exit",
+            Action = exitGame,
+            Size = new Vector2(160, 40),
+            Position = new Vector2(-180, 0),
+            Colour = Color4.HotPink
+        });
+
+        AddInternal(buttonContainer);
+    }
+
+
+    private void startGame() {
+
+    }
+
+    private void openSettings() {
+
+    }
+
+    private void exitGame() {
+
+    }
+}
